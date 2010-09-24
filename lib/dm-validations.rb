3e1edeaa81930d349b20bf68e0e8b7576326a3ae
Jordan Ritter <jpr5@darkridge.com>
2010-09-23 18:23:03 -0700

    Fix silly indentation problem I introduced in 6f2dca6f.

--- lib/dm-validations.rb
+++ lib/dm-validations.rb
@@ -26,18 +26,18 @@ rescue LoadError
 end
 
 class Object
-   # If receiver is callable, calls it and
-   # returns result. If not, just returns receiver
-   # itself
-   #
-   # @return [Object]
-   def try_call(*args)
-     if self.respond_to?(:call)
-       self.call(*args)
-     else
-       self
-     end
-   end
+  # If receiver is callable, calls it and
+  # returns result. If not, just returns receiver
+  # itself
+  #
+  # @return [Object]
+  def try_call(*args)
+    if self.respond_to?(:call)
+      self.call(*args)
+    else
+      self
+    end
+  end
 end
 
 module DataMapper

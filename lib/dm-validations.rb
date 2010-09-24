datamapper/dm-validations

commit 6f2dca6fd1e8fe14744f520a71e314cb28e98052

Avoid unnecessary active_support/extlib collision
Previously, the code made the assumption that
AS >= 2.3.5 was being used, as core_ext/object/blank
was introduced in that version.  In cases where an
earlier version is used (e.g. 2.3.3), the result is
a partial load of AS and then Extlib's overrides,
because the LoadError will trigger due to a missing
require.

When used with inside a Rails <2.3.5 app, chances
are AS has already been loaded completely anyway,
thus the LoadError consistently produces a "constant
redefined" warning regarding OrderedHash.

lib/dm-validations.rb
@@ -1,38 +1,43 @@
 require 'dm-core'
 
 begin
-
+  # We need array for extract_options! which attr_accessors uses, at least in AS
+  # 2.3.3.
+  require 'active_support/core_ext/array'
   require 'active_support/core_ext/class/attribute_accessors'
-  require 'active_support/core_ext/object/blank'
-  require 'active_support/ordered_hash'
-
-  class Object
-    # If receiver is callable, calls it and
-    # returns result. If not, just returns receiver
-    # itself
-    #
-    # @return [Object]
-    def try_call(*args)
-      if self.respond_to?(:call)
-        self.call(*args)
-      else
-        self
-      end
-    end
-  end
-
 rescue LoadError
-
   require 'extlib/class'
-  require 'extlib/dictionary'
+end
+
+begin
+  require 'active_support/core_ext/object/blank'
+rescue LoadError
   require 'extlib/blank'
-  require 'extlib/try_dup'
-  require 'extlib/object'
+end
 
-  module ActiveSupport
-    OrderedHash = Dictionary
+begin
+  require 'active_support/ordered_hash'
+rescue LoadError
+  require 'extlib/dictionary'
+
+  module ::ActiveSupport
+    OrderedHash = ::Dictionary
   end
+end
 
+class Object
+   # If receiver is callable, calls it and
+   # returns result. If not, just returns receiver
+   # itself
+   #
+   # @return [Object]
+   def try_call(*args)
+     if self.respond_to?(:call)
+       self.call(*args)
+     else
+       self
+     end
+   end
 end
 
 module DataMapper

commit 3e1edeaa81930d349b20bf68e0e8b7576326a3ae

Fix silly indentation problem I introduced in 6f2dca6f.

lib/dm-validations.rb
@@ -26,18 +26,18 @@ rescue LoadError
 end
 
 class Object
-   # If receiver is callable, calls it and
-   # returns result. If not, just returns receiver
-   # itself
-   #
-   # @return [Object]
-   def try_call(*args)
-     if self.respond_to?(:call)
-       self.call(*args)
-     else
-       self
-     end
-   end
+  # If receiver is callable, calls it and
+  # returns result. If not, just returns receiver
+  # itself
+  #
+  # @return [Object]
+  def try_call(*args)
+    if self.respond_to?(:call)
+      self.call(*args)
+    else
+      self
+    end
+  end
 end
 
 module DataMapper