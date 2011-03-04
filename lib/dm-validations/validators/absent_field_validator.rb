e0354c40075ffbdbc07f78bff71836b08d97ada4
Postmodern <postmodern.mod3@gmail.com>
2011-03-03 21:51:56 -0800

    80 columns and spacing.

--- lib/dm-validations/validators/absent_field_validator.rb
+++ lib/dm-validations/validators/absent_field_validator.rb
@@ -1,28 +1,30 @@
 module DataMapper
   module Validations
-
-    ##
     #
     # @author Guy van den Berg
     # @since  0.9
     class AbsenceValidator < GenericValidator
+
       def call(target)
         return true if target.validation_property_value(field_name).blank?
 
-        error_message = self.options[:message] || ValidationErrors.default_error_message(:absent, field_name)
-        add_error(target, error_message, field_name)
+        error_message = (
+          self.options[:message] || ValidationErrors.default_error_message(
+            :absent, field_name
+          )
+        )
 
+        add_error(target, error_message, field_name)
         false
       end
+
     end # class AbsenceValidator
 
     module ValidatesAbsence
-
       extend Deprecate
 
-      ##
-      # Validates that the specified attribute is "blank" via the attribute's
-      # #blank? method.
+      # Validates that the specified attribute is "blank" via the
+      # attribute's #blank? method.
       #
       # @note
       #   dm-core's support lib adds the #blank? method to many classes,
@@ -46,12 +48,14 @@ module DataMapper
       #   end
       #
       def validates_absence_of(*fields)
-        opts = opts_from_validator_args(fields)
-        add_validator_to_context(opts, fields, DataMapper::Validations::AbsenceValidator)
+        add_validator_to_context(
+          opts_from_validator_args(fields),
+          fields,
+          DataMapper::Validations::AbsenceValidator
+        )
       end
 
       deprecate :validates_absent, :validates_absence_of
-
     end # module ValidatesAbsent
   end # module Validations
 end # module DataMapper
