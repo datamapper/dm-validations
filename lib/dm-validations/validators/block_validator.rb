8b231ed2edc789f22f8105dc09c3dc97aa2f5e46
Postmodern <postmodern.mod3@gmail.com>
2011-03-03 21:55:32 -0800

    Style.

--- lib/dm-validations/validators/block_validator.rb
+++ lib/dm-validations/validators/block_validator.rb
@@ -1,13 +1,8 @@
 module DataMapper
   module Validations
-
-    ##
-    #
     # @author teamon
     # @since  0.9
     module ValidatesWithBlock
-
-      ##
       # Validate using the given block. The block given needs to return:
       # [result::<Boolean>, Error Message::<String>]
       #
@@ -46,15 +41,24 @@ module DataMapper
       def validates_with_block(*fields, &block)
         @__validates_with_block_count ||= 0
         @__validates_with_block_count += 1
+
         # create method and pass it to MethodValidator
-        raise ArgumentError.new('You need to pass a block to validates_with_block method') unless block_given?
+        unless block_given?
+          raise ArgumentError.new('You need to pass a block to validates_with_block method')
+        end
+
         method_name = "__validates_with_block_#{@__validates_with_block_count}".to_sym
         define_method(method_name, block)
+
         opts = opts_from_validator_args(fields)
         opts[:method] = method_name
-        add_validator_to_context(opts, fields.empty? ? [method_name] : fields, DataMapper::Validations::MethodValidator)
-      end
 
+        add_validator_to_context(
+          opts,
+          fields.empty? ? [method_name] : fields,
+          DataMapper::Validations::MethodValidator
+        )
+      end
     end # module ValidatesWithMethod
   end # module Validations
 end # module DataMapper
