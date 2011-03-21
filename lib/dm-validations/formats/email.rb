b086a2e6152e156d2c5700c0f0619da3e7f6fe1c
Dan Kubb <dan.kubb@gmail.com>
2011-03-20 19:02:34 -0700

    Style. Add two spaces before inline comments

--- lib/dm-validations/formats/email.rb
+++ lib/dm-validations/formats/email.rb
@@ -30,14 +30,14 @@ module DataMapper
             # http://jira.codehaus.org/browse/JRUBY-5622
             alpha = 'a-zA-Z'
           else
-            alpha = 'a-zA-Z\p{L}' # Changed from RFC2822 to include unicode chars
+            alpha = 'a-zA-Z\p{L}'  # Changed from RFC2822 to include unicode chars
           end
           digit          = '0-9'
           atext          = "[#{alpha}#{digit}\!\#\$\%\&\'\*+\/\=\?\^\_\`\{\|\}\~\-]"
-          dot_atom_text  = "#{atext}+([.]#{atext}*)+" # Last char changed from * to +
+          dot_atom_text  = "#{atext}+([.]#{atext}*)+"  # Last char changed from * to +
           dot_atom       = dot_atom_text
           no_ws_ctl      = '\x01-\x08\x11\x12\x14-\x1f\x7f'
-          qtext          = "[^#{no_ws_ctl}\\x0d\\x22\\x5c]" # Non-whitespace, non-control character except for \ and "
+          qtext          = "[^#{no_ws_ctl}\\x0d\\x22\\x5c]"  # Non-whitespace, non-control character except for \ and "
           text           = '[\x01-\x09\x11\x12\x14-\x7f]'
           quoted_pair    = "(\\x5c#{text})"
           qcontent       = "(?:#{qtext}|#{quoted_pair})"
@@ -49,7 +49,7 @@ module DataMapper
           dtext          = "[#{no_ws_ctl}\\x21-\\x5a\\x5e-\\x7e]"
           dcontent       = "(?:#{dtext}|#{quoted_pair})"
           domain_literal = "\\[#{dcontent}+\\]"
-          obs_domain     = "#{atom}([.]#{atom})+" # Last char changed from * to +
+          obs_domain     = "#{atom}([.]#{atom})+"  # Last char changed from * to +
           domain         = "(?:#{dot_atom}|#{domain_literal}|#{obs_domain})"
           addr_spec      = "#{local_part}\@#{domain}"
           pattern        = /\A#{addr_spec}\z/u
