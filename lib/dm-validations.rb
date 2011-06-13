31a224340df4328274ab4100f68c1f83976210cc
Emmanuel Gomez <emmanuel.gomez@gmail.com>
2011-06-12 23:38:23 -0700

    Move Object#try_call to support/object.rb.

--- lib/dm-validations.rb
+++ lib/dm-validations.rb
@@ -1,22 +1,6 @@
 require 'dm-core'
 require 'dm-validations/support/ordered_hash'
 
-class Object
-  # If receiver is callable, calls it and
-  # returns result. If not, just returns receiver
-  # itself
-  #
-  # @return [Object]
-  # @api private
-  def try_call(*args)
-    if self.respond_to?(:call)
-      self.call(*args)
-    else
-      self
-    end
-  end
-end
-
 require 'dm-validations/exceptions'
 require 'dm-validations/validation_errors'
 require 'dm-validations/contextual_validators'
